f3797fa9bea545b4d3d08b0004e3499b33c9dd92
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-04-25 10:52:38 +0100

    first test and implmentation of buzzwords

---
+++ apps/bingo/lib/bingo/buzzwords.ex
@@ -0,0 +1,16 @@
+defmodule Bingo.Buzzwords do
+
+  def read_buzzwords(file_path) do
+    file_path
+     |> Path.expand(__DIR__)
+     |> File.read!()
+     |> String.split("\n", trim: true)
+     |> Enum.map(&String.split(&1, ","))
+     |> Enum.map(fn [phrase, points] ->
+                        %{phrase: phrase, points: String.to_integer(points)}
+                      end )
+
+
+  end
+
+end
---
+++ apps/bingo/test/buzzwords_test.exs
@@ -0,0 +1,16 @@
+defmodule BuzzwordsTest do
+  use ExUnit.Case, async: true
+  doctest Bingo.Buzzwords
+
+  alias Bingo.Buzzwords
+
+  test "read buzzwords csv files" do
+    buzzwords = "buzzwords_test.csv"
+                  |> Path.expand(__DIR__)
+                  |> Buzzwords.read_buzzwords
+
+    assert [%{phrase: "Above My Pay Grade", points: 400 }, %{phrase: "Action Item", points: 350}] = buzzwords
+  end
+
+
+end
