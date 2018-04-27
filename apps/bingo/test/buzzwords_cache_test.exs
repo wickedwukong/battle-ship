dcbf5542108ced3a15b50756b1b82c6aa604cf64
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-04-27 22:49:01 +0100

    my first gen server. it is used to keep buzzwords as a state

---
+++ apps/bingo/test/buzzwords_cache_test.exs
@@ -0,0 +1,16 @@
+defmodule BuzzwordsCacheTest do
+  use ExUnit.Case, async: true
+  doctest Bingo.BuzzwordsCache
+
+  alias Bingo.BuzzwordsCache
+
+  test "give the cached buzzwords" do
+    "buzzwords_test.csv"
+      |> Path.expand(__DIR__)
+      |> BuzzwordsCache.start_link()
+
+    cached_buzzwords = BuzzwordsCache.get_buzzwords()
+
+    assert %{phrase: _, points: _} = List.first(cached_buzzwords)
+  end
+end
