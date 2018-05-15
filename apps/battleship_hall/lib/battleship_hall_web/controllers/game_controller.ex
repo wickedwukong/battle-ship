f60e4361c9d1f38cbcd445ea0695dcc4171fb981
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-05-15 12:38:26 +0100

    make the new game page working

--- apps/battleship_hall/lib/battleship_hall_web/controllers/game_controller.ex
+++ apps/battleship_hall/lib/battleship_hall_web/controllers/game_controller.ex
@@ -4,7 +4,10 @@ defmodule BattleshipHallWeb.GameController do
   plug(:require_player)
 
   def new(conn, _params) do
-    render(conn, "new.html")
+    conn
+    |> assign(:game_name, "game_name")
+    |> assign(:auth_token, generate_auth_token(conn))
+    |> render("new.html")
   end
 
   defp require_player(conn, _opts) do
@@ -16,5 +19,10 @@ defmodule BattleshipHallWeb.GameController do
       |> redirect(to: session_path(conn, :new))
       |> halt()
     end
+  end
+
+  defp generate_auth_token(conn) do
+    current_player = get_session(conn, :current_player)
+    Phoenix.Token.sign(conn, "player auth", current_player)
   end
 end
