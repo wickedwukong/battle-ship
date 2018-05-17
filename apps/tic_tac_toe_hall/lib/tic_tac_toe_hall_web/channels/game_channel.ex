43cc0419e308c2e1cef56bee0a41bba7ca27975a
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2018-05-17 23:48:52 +0100

    handle push message from client and allow channel to join

---
+++ apps/tic_tac_toe_hall/lib/tic_tac_toe_hall_web/channels/game_channel.ex
@@ -0,0 +1,39 @@
+defmodule TicTacToeHallWeb.GameChannel do
+  use TicTacToeHallWeb, :channel
+
+  alias TicTacToe.GameServer
+
+  def join("games:" <> game_name, _params, socket) do
+    case GameServer.game_pid(game_name) do
+      pid when is_pid(pid) ->
+        send(self(), {:after_join, game_name})
+        {:ok, socket}
+
+      nil ->
+        {:error, %{reason: "Game does not exist: #{game_name}"}}
+    end
+  end
+
+  def handle_info({:after_join, game_name}, socket) do
+    push(socket, "game_summary", %{game_summary: "This is a place holder for game summary"})
+
+    {:noreply, socket}
+  end
+
+  def handle_in("mark_square", %{"square_x" => x, "square_y" => y}, socket) do
+    "games:" <> game_name = socket.topic
+
+    case GameServer.game_pid(game_name) do
+      pid when is_pid(pid) ->
+        //TODO: implement mark square
+        broadcast!(socket, "game_summary", %{
+          game_summary: "This is a place holder for game summary"
+        })
+
+        {:reply, :ok, socket}
+
+      nil ->
+        {:error, %{reason: "Game does not exist: #{game_name}"}}
+    end
+  end
+end
